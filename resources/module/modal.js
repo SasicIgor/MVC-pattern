1e37e26ece8bea48cbf1b14d0cd1edf87ea57c16
Igor-Sasic <igor.sasic811@gmail.com>
2024-01-13 20:17:07 +0100

    nothing added

--- resources/module/modal.js
+++ resources/module/modal.js
@@ -1,38 +1,38 @@
-const model=(function(){
+const model = (function () {
 
-    const StorageItem=function(id,name,quantity,measurement,type){
-        this.id=id;
-        this.name=name;
-        this.quantity=quantity;
-        this.measurement=measurement;
-        this.type=type;
+    const StorageItem = function (id, name, quantity, measurement, type) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.measurement = measurement;
+        this.type = type;
     }
 
 
-    let storage={
-        
-        allCoolers:{
-            1:[],
-            2:[],
-            3:[],
-            4:[],
-            5:[],
+    let storage = {
+
+        allCoolers: {
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
         },
     }
 
     return {
 
-        addItem:function(name,quantity,measurement,type){
+        addItem: function (name, quantity, measurement, type) {
+            console.log(storage)
+            let id, item;
 
-            let id,item;
-
-            if(storage.allCoolers[type].length>0){
-                id=storage.allCoolers[type][storage.allCoolers[type].length-1].id+1
+            if (storage.allCoolers[type].length > 0) {
+                id = storage.allCoolers[type][storage.allCoolers[type].length - 1].id + 1
             }
-            else{
-                id=1;
+            else {
+                id = 1;
             }
-            item=new StorageItem(id,name,quantity,measurement,type)
+            item = new StorageItem(id, name, quantity, measurement, type)
             storage.allCoolers[type].push(item)
 
             return item;
